514b8d58eeccccd27db201a31a2addd46006bc8d
Mohdoo <marinszcz@gmail.com>
2022-04-11 22:06:00 +0200

    Fix empty leaderboard

--- commandes/leaderboard.js
+++ commandes/leaderboard.js
@@ -22,17 +22,17 @@ exports.procedure = async (interaction) => {
     }
 
     // ajoute des scores par défaut s’il y a moins de trois joueurs enregistrés
-    if (podium.length < 3) {
-        // le héros légendaire
-        podium.unshift({ name: "Roto", id: 3, score: 3333, percentage: 100 });
+    if (podium.length < 1) {
+        // il vient juste de commencer à jouer mais ne vous inquiétez pas il va les rattraper
+        podium.unshift({ name: "Solo", id: 4, score: 444, percentage: 0 });
     }
     if (podium.length < 2) {
         // sans doute le héros le plus aimé
         podium.unshift({ name: "Eight", id: 8, score: 888, percentage: 50 });
     }
-    if (podium.length < 1) {
-        // il vient juste de commencer à jouer mais ne vous inquiétez pas il va les rattraper
-        podium.unshift({ name: "Solo", id: 4, score: 444, percentage: 0 });
+    if (podium.length < 3) {
+        // le héros légendaire
+        podium.unshift({ name: "Roto", id: 3, score: 3333, percentage: 100 });
     }
 
     const embed = new MessageEmbed()
